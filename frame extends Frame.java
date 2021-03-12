a055cf6940fbc4f69f11c8581918da0e4a389c82
Prateekk56 <80531979+Prateekk56@users.noreply.github.com>
2021-03-12 20:22:42 +0530

    Create frame extends Frame.java

---
+++ frame extends Frame.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+public class frame extends Frame {
+	public static void main(String[] args) {
+		Frame f=new Frame("Third Frame");
+		f.setSize(500,500);
+		f.setVisible(true);
+	}
+
+}
