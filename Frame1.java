377c444f05ea2d2bff5ec99d0289116921710615
Prateekk56 <80531979+Prateekk56@users.noreply.github.com>
2021-03-12 20:19:55 +0530

    Update and rename README.md to Frame1.java

---
+++ Frame1.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+public class Frame1 {
+
+	public static void main(String[] args) {
+		Frame f1=new Frame("First Frame ");
+		f1.setSize(2000,2050);
+		f1.setVisible(true);
+	}
+
+}
+
