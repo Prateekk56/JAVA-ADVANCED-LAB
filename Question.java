9e7f8907b73967edd2999464db2dcb2a1290cdaa
Prateekk56 <80531979+Prateekk56@users.noreply.github.com>
2021-03-12 20:21:11 +0530

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+class Question {
+	
+	public static void main(String[] args) {
+		Frame f=new Frame();
+		f.setSize(500,100);
+		f.setVisible(true);
+		f.setTitle("Second Frame");
+	}	
+
+}
